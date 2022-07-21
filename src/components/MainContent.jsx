1e48ff54b90274532b5266ab5920daa4f273af35
Jobin Biju <jobinbiju9090@gmail.com>
2022-07-21 20:43:38 +0530

    Main ui btns added

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -6,10 +6,14 @@ function MainContent() {
     <div className="main">
       <div className="main-text">
         <h1>
-          Best <strong className='text-grad'>Educational</strong>
+          Best <strong className="text-grad">Educational</strong>
         </h1>
         <h1>VR Games</h1>
         <h1>For Your Kids</h1>
+        <div className="main-btns">
+          <h6 className="btn-1">Explore Now</h6>
+          <h6 className="btn-2">Browse Store</h6>
+        </div>
       </div>
       <img className="vr-img" src={vr} alt="vr-1" />
     </div>
